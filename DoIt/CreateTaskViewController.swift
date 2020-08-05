d2999cc79bd851765a3fee3e3ded2316835b28aa
Ben Leimbach <benleimbach@gmail.com>
2020-08-04 22:01:56 -0600

    somehow it works

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Ben Leimbach on 8/3/20.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        // Create a Task from the outlet information
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        // Add new task to array in previous viewController
+        
+        previousVC.tasks.append(task)
+        
+    }
+    
+ }
